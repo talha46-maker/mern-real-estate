e83b7914cdc56ffda9346d1714dfb8aeb0a14b9f
talha46-maker <talha.tb.786@gmail.com>
2024-02-25 18:38:53 +0500

    sign in api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
+/*
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
-
+import  JWT  from "jsonwebtoken";
 
 
 export const signup = async (req,res,next)=>{
@@ -22,4 +23,62 @@ res.status(201).json('user created successfully!');
 
 }
 
+};
+export const signin = async (req,res,next)=>{
+const {email,password}=req.body;
+try {
+    const ValidUser = await User.findOne({ email});
+    if (!ValidUser) return next(errorHandler(404,'user not found!'));
+    const validPassword= bcryptjs.compareSync(password, ValidUser.password);
+    if(!validPassword) return next(errorHandler(401,'wrong credentials  !'));
+    const token =jwt.sign({id:ValidUser._id }, process.env.JWT_SECRET)
+    res.cookie('access_token',token, {httpOnly:true})
+    .status(200)
+    .json(ValidUser);
+} catch (error) {
+    next(error);
+}
+}
+*/
+
+
+
+
+
+
+
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import JWT from "jsonwebtoken"; // Corrected import name to JWT
+
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const salt = await bcryptjs.genSalt(12);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+        await newUser.save();
+        res.status(201).json('user created successfully!');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'user not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'wrong credentials  !'));
+        const token = JWT.sign({ id: validUser._id }, process.env.JWT_SECRET); // Corrected JWT usage
+        const{password:pass,...rest}=validUser._doc ;
+        res
+        .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
 };
